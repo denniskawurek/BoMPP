04489692b67ff349df2e91ab5702adfae513c2b7
Dennis Kawurek <dennis.kawurek@mni.thm.de>
2018-10-11 20:59:25 +0200

    Handle network errors.

--- src/main/java/de/dkwr/bompp/BotInitializer.java
+++ src/main/java/de/dkwr/bompp/BotInitializer.java
@@ -23,10 +23,13 @@ import de.dkwr.bompp.omemo.BotTrustCallback;
 import de.dkwr.bompp.util.BotLogger;
 import de.dkwr.bompp.omemo.MessageListener;
 import de.dkwr.bompp.omemo.OmemoController;
+import de.dkwr.bompp.omemo.XMPPConnectionManager;
+import de.dkwr.bompp.util.BotConfiguration;
 
 import java.io.File;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.ReconnectionManager;
 import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.chat2.ChatManager;
 import org.jivesoftware.smack.roster.Roster;
@@ -60,29 +63,35 @@ public class BotInitializer {
     /**
      * Initializes the XMPP connection and OMEMOManager for the bot with all
      * message listeners.
-     * @param jid the XMPP/Jabber Id of the bot
-     * @param pwd the corresponding password
-     * @param path the store path
+     * @param cfg Configuration for the bot
      * @param commandQueue the CommandQueue where the commands belong to
-     * @param enableXMPPDebugMode true|false whether the XMPP debug mode provided by smack shall be enabled
      */
-    public void init(String jid, char[] pwd, String path, CommandQueue commandQueue, boolean enableXMPPDebugMode) {
+    public void init(BotConfiguration cfg, CommandQueue commandQueue) {
         try {
             System.out.println("Start initializing BoMPP please wait...");
-            SmackConfiguration.DEBUG = enableXMPPDebugMode;
+            SmackConfiguration.DEBUG = cfg.getEnableXMPPDebugMode();
             OmemoConfiguration.setAddOmemoHintBody(false);
 
-            this.connection = new XMPPTCPConnection(jid, new String(pwd));
+            this.connection = new XMPPTCPConnection(cfg.getJID(), new String(cfg.getPassword()));
+            this.connection.addConnectionListener(new XMPPConnectionManager());
+            
+            ReconnectionManager reconnectionManager = ReconnectionManager.getInstanceFor(this.connection);
+            reconnectionManager.enableAutomaticReconnection();
+            reconnectionManager.setReconnectionPolicy(ReconnectionManager.ReconnectionPolicy.RANDOM_INCREASING_DELAY);
+            
             SignalOmemoService.acknowledgeLicense();
             SignalOmemoService.setup();
             SignalOmemoService service = (SignalOmemoService) SignalOmemoService.getInstance();
-            service.setOmemoStoreBackend(new SignalCachingOmemoStore(new SignalFileBasedOmemoStore(new File(path))));
+            service.setOmemoStoreBackend(new SignalCachingOmemoStore(new SignalFileBasedOmemoStore(new File(cfg.getStorePath()))));
             this.omemoManager = OmemoManager.getInstanceFor(connection);
             this.omemoManager.setTrustCallback(new BotTrustCallback());
+            
             this.omemoStore = OmemoService.getInstance().getOmemoStoreBackend();
+            
             this.connection.setReplyTimeout(10000);
             this.connection = connection.connect();
             this.connection.login();
+            
             this.omemoManager.initialize();
 
             CarbonManager.getInstanceFor(connection).enableCarbons();
